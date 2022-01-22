64818c809941b837eef4b4442f4a9d3e25b9d17e
Sara <sara.gonzalezsa@gmail.com>
2022-01-22 23:35:47 +0100

    created register, an modified app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+let botonDelete = document.getElementById("Eliminar");
+
+function remove_card() {
+  let list = document.getElementById('card');
+  elem.parentNode.removeChild(elem);
+    return false;
+}
